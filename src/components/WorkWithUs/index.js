d1bd7107d773078a6c19bf2a4817cb6842595202
NeelimaAitha <138878450+NeelimaAitha@users.noreply.github.com>
2024-02-20 11:56:42 +0530

    Update index.js

--- src/components/WorkWithUs/index.js
+++ src/components/WorkWithUs/index.js
@@ -1,56 +1,78 @@
-import {useRef, useEffect} from 'react'
+import { useRef, useEffect, useState } from "react";
+import { gsap } from "gsap";
+import { useIntersection } from "react-use";
 
-import imageUrl from './yourImage.jpg';
-import { gsap } from 'gsap';
+import WorkWithUsRightCardItem from "../WorkWithUsRightCardItem";
 
-import WorkingWithUsRightCardItem from '../WorkingWithUsRightCardItem'
-
-import './index.css'
+import "./index.css";
 
 const workWithUsLeftCardDetails = [
   {
     id: 1,
-    title: 'Power through, even when the going gets tough',
+    title: "Power through, even when the going gets tough",
     description:
-      'We help you spot and work around whatever stands in the way, be it bad habits, fears, etc.',
+      "We help you spot and work around whatever stands in the way, be it bad habits, fears, etc.",
   },
   {
     id: 2,
-    title: 'Power through, even when the going gets tough',
+    title: "Power through, even when the going gets tough",
     description:
-      'We help you spot and work around whatever stands in the way, be it bad habits, fears, etc.',
+      "We help you spot and work around whatever stands in the way, be it bad habits, fears, etc.",
   },
   {
     id: 3,
-    title: 'Power through, even when the going gets tough',
+    title: "Power through, even when the going gets tough",
     description:
-      'We help you spot and work around whatever stands in the way, be it bad habits, fears, etc.',
+      "We help you spot and work around whatever stands in the way, be it bad habits, fears, etc.",
   },
   {
     id: 4,
-    title: 'Power through, even when the going gets tough',
+    title: "Power through, even when the going gets tough",
     description:
-      'We help you spot and work around whatever stands in the way, be it bad habits, fears, etc.',
+      "We help you spot and work around whatever stands in the way, be it bad habits, fears, etc.",
   },
-]
+];
 
 const WorkWithUs = () => {
-  const workWithTitleRef = useRef(null)
-  const workWithSideTitleRef = useRef(null)
- const imageRef = useRef(null);
-const imageRef = useRef(null);
+  const workWithTitleRef = useRef(null);
+  const workWithSideTitleRef = useRef(null);
+  const workWithUsImage = useRef(null);
+  const [animated, setAnimated] = useState(false);
+  const intersection = useIntersection(workWithTitleRef, {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.3,
+  });
+  const element = workWithUsImage.current;
+  const tl = gsap.timeline();
 
   useEffect(() => {
-    const image = imageRef.current;
+    if (intersection && intersection.intersectionRatio > 0.3 && !animated) {
+      gsap.from(workWithTitleRef.current, {
+        duration: 1,
+        x: -70,
+        ease: "power2.in",
+      });
+      gsap.from(workWithSideTitleRef.current, {
+        duration: 1,
+        x: 70,
+        ease: "power2.in",
+      });
+      tl.to(element, {
+        duration: 1,
+        x: "20%",
+        ease: "power1.inOut",
+      }).to(element, {
+        duration: 1,
+        x: "-20%",
+        ease: "power1.inOut",
+      });
 
-    gsap.fromTo(
-      image,
-      { x: '-100%' },
-      { x: '0%', duration: 2, ease: 'power3.out' }
-    );
-  }, []);
-
-  return <img ref={imageRef} src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNUHHhmODkp6LXZiHf1FRJNpV3ukrlxjHgNMQTNQ46WNCTL5RV0ACYYWt_HrbfX08YLms&usqp=CAU"} alt="Animated" className="animated-image" />;
+      setAnimated(true);
+    } else if (intersection && intersection.intersectionRatio === 0) {
+      setAnimated(false);
+    }
+  }, [intersection, animated]);
 
   return (
     <div className="work-with-us-container">
@@ -65,6 +87,12 @@ const imageRef = useRef(null);
       <div className="work-with-us-cards">
         <div className="work-with-us-left-card">
           <div className="about-card">
+            <img
+              src="https://self-awareness.ahead-app.com/static/media/home_owel.e23a16a0.svg"
+              className="work-with-us-image"
+              alt="meet ahead"
+              ref={workWithUsImage}
+            />
             <p className="about-product-text">About</p>
             <p className="product-about-description">
               At ahead our goal is to make self improvement fun and lasting. We
@@ -82,8 +110,8 @@ const imageRef = useRef(null);
           </div>
         </div>
         <ul className="work-with-us-right-card">
-          {workWithUsLeftCardDetails.map(leftCardItem => (
-            <WorkingWithUsRightCardItem
+          {workWithUsLeftCardDetails.map((leftCardItem) => (
+            <WorkWithUsRightCardItem
               key={leftCardItem.id}
               cardDetails={leftCardItem}
             />
@@ -91,7 +119,7 @@ const imageRef = useRef(null);
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default WorkWithUs
+export default WorkWithUs;
